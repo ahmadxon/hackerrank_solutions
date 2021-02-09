4fe79c8173d6e3d1bb64c93039f731780915d21b
Ahmadhon <ahmadxonk@youvr.io>
2021-02-09 16:05:37 +0900

    Create Solution.java

---
+++ Java/Sparse Arrays/Solution.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String[] a = new String[N];
+        for(int i=0; i<N; i++){
+            a[i] = sc.next();
+        }
+        
+        int Q = sc.nextInt();
+        int[] result = new int[Q];
+        int i=0;
+        
+        while(sc.hasNext()){
+            String query = sc.next();
+            int count = 0;
+            for(int j=0; j<N; j++){
+                if (a[j].equals(query)){
+                    count++;
+                }
+            }
+            result[i++] = count;
+        }
+        
+        for(i=0; i<Q; i++){
+            System.out.println(result[i]);
+        }
+    }
+}
